3230472a3d3ed00af76a2c504182691314dd95d1
toshi <k08045kk+toshi@gmail.com>
2020-01-17 12:20:08 +0900

    update FID対策+他
    FID対策として、setTimeoutでlazy処理を更に遅延
    FID対策として、addEventListener()にoptionsを設定
    イベント種類を変更、スクロール位置の取得方法変更

--- onLazy.js
+++ onLazy.js
@@ -1,3 +1,4 @@
+/*! onLazy.js v1.3 | MIT License | github.com/k08045kk/onLazy.js */
 /**
  * onLazy.js
  * 遅延イベントリスナー
@@ -6,35 +7,56 @@
  * loadイベント以前にユーザーイベントが発火した場合、loadイベント時に発火します。
  * loadイベント時にドキュメント先頭にない場合も発火します。
  * イベントは、一度しか発生しません。
+ * 注意：初回ユーザイベントより後に lazy は発火します。
+ *       初回ユーザイベント時に必須の処理は、lazy で処理しないでください。
  * 登録：window.addEventListener('lazy', func);
  * 対応：IE9+
  * @auther      toshi(https://www.bugbugnow.net/p/profile.html)
- * @license     MIT License
- * @see         https://opensource.org/licenses/MIT
- * @version     1
- * @see         1 - add - 初版
- * @see         https://github.com/k08045kk/onLazy.js
+ * @version     1.3
+ * @see         1 - 20190601 - add - 初版
+ * @see         1.1 - 20200116 - update - FID対策として、setTimeoutでlazy処理を更に遅延
+ * @see         1.2 - 20200117 - update - FID対策として、addEventListener()にoptionsを設定
+ * @see         1.3 - 20200117 - update - イベント種類を変更、スクロール位置の取得方法変更
  */
 (function(win, doc) {
   'use strict';
   
+  var delay = 10;
   var lazy = false;
   var load = false;
   var fire = false;
-  // scrollは保険。wheel/touchstartでスクロールを検出可能。
-  // keyupは保険。キー押下中にページ遷移するとkeydownを検出せずに、keyupのみ検出する。
-  // mouseoverで、mosedown/mousemoveを検出する。
-  var types = ['wheel','keydown','keyup','mouseover','touchstart','scroll'];
+  // click: 保険。mouseover/touchstart でスクロールを検出可能
+  // scroll: 保険。wheel/touchstart でスクロールを検出可能
+  // mouseover: mosedown/mousemove を検出
+  // 想定初回イベント: PC:mousemove(mouseover), SP:scroll(touchstart)
+  // 補足：PC環境では初回表示領域が大きいため、スクロールより早くしたい。なので、マウス移動を検出する
+  var types = ['click','mouseover','keydown','touchstart','pointerover','wheel','scroll'];
+  var options = {capture:true, once:true, passive:true};
   
-  function onLazy(event) {
+  function dispatchLazyEvent() {
+    // 遅延処理呼び出し
+    var evt;
+    var data = void 0;
+    try {
+      evt = new CustomEvent('lazy', {detail:data});
+    } catch (e) {
+      // IE11-9
+      evt = doc.createEvent('CustomEvent');
+      evt.initCustomEvent('lazy', true, true, data);
+    }
+    win.dispatchEvent(evt);
+    //console.log('lazy: dispatch');
+  }
+  
+  function onLazy(event, isLoad) {
     if (fire === false) {
       // 初回イベントでリスナー解除（load前の複数回呼び出しを回避）
       // load前：loadで遅延処理実行
       // load後：このまま遅延処理実行
       fire = true;
       //console.log('lazy: fire');
-      for (var i=0; i<types.length; i++) {
-        win.removeEventListener(types[i], onLazy);
+      for (var i=0, len=types.length; i<len; i++) {
+        win.removeEventListener(types[i], onLazy, options);
       }
     }
     if (lazy === false && load === true) {
@@ -42,35 +64,30 @@
       lazy = true;
       //console.log('lazy: lazy');
       
-      // 遅延処理呼び出し
-      var evt;
-      var data = void 0;
-      try {
-        evt = new CustomEvent('lazy', {detail: data});
-      } catch (e) {
-        // IE11-9
-        evt = doc.createEvent('CustomEvent');
-        evt.initCustomEvent('lazy', true, true, data);
+      if (isLoad === true) {
+        dispatchLazyEvent();
+      } else {
+        // 更に遅延する。FID対策
+        win.setTimeout(dispatchLazyEvent, delay);
       }
-      win.dispatchEvent(evt);
-      //console.log('lazy: dispatch');
     }
   }
   
-  for (var i=0; i<types.length; i++) {
-    win.addEventListener(types[i], onLazy);
+  for (var i=0, len=types.length; i<len; i++) {
+    win.addEventListener(types[i], onLazy, options);
   }
   win.addEventListener('load', function(event) {
     // 既に着火済み or ドキュメントの途中（更新時 or ページ内リンク）
     load = true;
     //console.log('lazy: load');
-    if (fire === true || doc.documentElement.scrollTop != 0 || doc.body.scrollTop != 0) {
+    
+    var y =  win.pageYOffset || doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+    if (fire === true || y != 0) {
       //console.log('lazy: fire: '+fire);
-      //console.log('lazy: scroll: '+doc.documentElement.scrollTop);
-      //console.log('lazy: scroll: '+doc.body.scrollTop);
-      onLazy(event);
+      //console.log('lazy: scroll: '+y);
+      onLazy(event, true);
     }
     //console.log('lazy: loaded');
-  });
+  }, options);
   //console.log('lazy: init');
 })(window, document);
