8ee3afb8ac978ca058bfedd8e70757144c9d3703
toshi <k08045kk+toshi@gmail.com>
2020-02-19 14:46:59 +0900

    update v1.8 FID対策
    FID対策として、バブリングフェーズまで待機するように変更
    FID対策として、イベント種別変更（over -> down, move）
    lazyイベントをバブリングなし・キャンセル不可とする
    setTimeoutでのlazy処理を削除

--- onLazy.js
+++ onLazy.js
@@ -1,19 +1,18 @@
-/*! onLazy.js v1.7 | MIT License | https://github.com/k08045kk/onLazy.js/blob/master/LICENSE */
+/*! onLazy.js v1.8 | MIT License | https://github.com/k08045kk/onLazy.js/blob/master/LICENSE */
 /**
  * onLazy.js
- * 遅延イベントリスナー
- * カスタムイベントとして「初回ユーザーイベント後に発火する」イベントを追加します。
+ * カスタムイベントとして「初回ユーザーイベント後に発火する」遅延カスタムイベントを追加します。
  * loadイベント以降の初回ユーザーイベント後に発火します。
  * loadイベント以前にユーザーイベントが発火した場合、loadイベント時に発火します。
  * loadイベント時にドキュメント先頭でない場合も発火します。
- * イベントは、一度しか発生しません。
+ * lazyイベントは、一度しか発生しません。
  * 注意：初回ユーザイベントより後に lazy は発火します。
  *       初回ユーザイベントは、取り逃す前提で処理してください。
- * 注意：onLazy.jsの実行、カスタムイベントへの登録は、loadイベントより前に実施してください。
+ * 注意：カスタムイベント登録は、「loadイベントより前」または「onLazy.js実行より前」に実施して下さい。
  * 登録：window.addEventListener('lazy', func);
- * 対応：IE9+
+ * 対応：IE9+ (addEventListener, createEvent, initCustomEvent, pageYOffset)
  * @auther      toshi (https://github.com/k08045kk)
- * @version     1.7
+ * @version     1.8
  * @see         1 - 20190601 - add - 初版
  * @see         1.1 - 20200116 - update - FID対策として、setTimeoutでlazy処理を更に遅延
  * @see         1.2 - 20200117 - update - FID対策として、addEventListener()にoptionsを設定
@@ -22,87 +21,100 @@
  * @see         1.5 - 20200123 - update - toolazyを追加
  * @see         1.6 - 20200124 - update - リファクタリング（loadイベント時も遅延させる）
  * @see         1.7 - 20200201 - update - リファクタリング
+ * @see         1.8 - 20200209 - update - FID対策として、バブリングフェーズまで待機するように変更
+ * @see         1.8 - 20200215 - update - FID対策として、イベント種別変更（over -> down, move）
+ * @see         1.8 - 20200215 - update - lazyイベントをバブリングなし・キャンセル不可とする
+ * @see         1.8 - 20200215 - update - setTimeoutでのlazy処理を削除
  */
-(function(window, document) {
+(function(document) {
   'use strict';
   
-  var delay = 10;
   var lazy = false;
   var load = false;
   var fire = false;
-  // click: 保険。mouseover/touchstart でスクロールを検出可能
-  // scroll: 保険。wheel/touchstart でスクロールを検出可能
-  // mouseover: mosedown/mousemove を検出
-  // 想定初回イベント: PC:mousemove(mouseover), SP:scroll(touchstart)
-  // 補足：PC環境では初回表示領域が大きいため、スクロールより早くしたい。なので、マウス移動を検出する
-  var types = ['click','mouseover','keydown','touchstart','pointerover','wheel','scroll'];
-  // capture=true: 何故かfalseよりFID良い。理由は不明
-  var options = {capture:true, once:true, passive:true};
+  // イベント種類
+  // lazyイベントは、より早く発火することが望ましいが、FIDに悪影響を与えるべきではない。
+  // そのため、mouseover/pointeroverではなく、mosedown/pointerdownとする。
+  // 想定初回イベント: PC:mousedown/mousemove/scroll, SP:touchstart(scroll)
+  // 補足：PC環境は表示領域が大きいため、スクロールより早く実行したい。そのため、マウス移動を検出する
+  var types = ['click','mousedown','keydown','touchstart','pointerdown','mousemove','pointermove','scroll'];
+  // バブリングフェーズ、1回のみ、中断なし
+  var options = {capture:false, once:true, passive:true};
   
-  function dispatchLazyEvent(opt_type) {
-    var type = opt_type != null ? opt_type: 'lazy';
-    
-    // 遅延処理呼び出し
+  var eachEventListener = function(callback) {
+    for (var i=0, len=types.length; i<len; i++) {
+      callback(types[i], onLazy, options);
+    }
+  };
+  
+  // カスタムイベントの発信
+  var dispatchCustomEvent = function(type) {
     var evt;
     var data = void 0;
     try {
-      evt = new CustomEvent(type, {detail:data});
+      // バブルアップなし、キャンセル不可
+      evt = new CustomEvent(type, {detail:data, bubbles:false, cancelable:false});
     } catch (e) {
       // IE11-9
       evt = document.createEvent('CustomEvent');
-      evt.initCustomEvent(type, true, true, data);
+      evt.initCustomEvent(type, false, false, data);
     }
-    window.dispatchEvent(evt);
+    dispatchEvent(evt);
     //console.log('lazy: dispatch');
-  }
+  };
   
-  function onLazy() {
+  var onLazy = function() {
     if (!fire) {
       // 初回イベントでリスナー解除（load前の複数回呼び出しを回避）
       // load前：loadで遅延処理実行
       // load後：このまま遅延処理実行
       fire = true;
       //console.log('lazy: fire');
-      for (var i=0, len=types.length; i<len; i++) {
-        window.removeEventListener(types[i], onLazy, options);
-      }
+      
+      eachEventListener(removeEventListener);
     }
     if (!lazy && load) {
       // 複数呼び出し回避
       lazy = true;
       //console.log('lazy: lazy');
       
-      // 更に遅延する。FID対策
-      window.setTimeout(dispatchLazyEvent, delay);
+      dispatchCustomEvent('lazy');
     }
-  }
+  };
   
-  // main
-  for (var i=0, len=types.length; i<len; i++) {
-    window.addEventListener(types[i], onLazy, options);
-  }
-  window.addEventListener('load', function() {
+  var onLoad = function() {
     load = true;
     //console.log('lazy: load');
     
-    // 既に発火済み or ドキュメントの途中（更新時 or ページ内リンク）
-    if (fire || window.pageYOffset) {
+    // 既に発火済み or ドキュメントの途中（更新時 or ページ内リンク時）
+    if (fire || pageYOffset) {
       //console.log('lazy: fire: '+fire);
-      //console.log('lazy: scroll: '+window.pageYOffset);
+      //console.log('lazy: scroll: '+pageYOffset);
       onLazy();
     }
     //console.log('lazy: loaded');
-  }, options);
-  window.addEventListener('unload', function() {
+  };
+  
+  var onUnload = function() {
     if (!lazy) {
       // 遅延イベント不発時のイベント
       // unload時のイベントのため、確実に処理されるとは保証できない
       lazy = true;
-      dispatchLazyEvent('toolazy');
+      dispatchCustomEvent('toolazy');
       // 注意：toolazy を予告なく名称変更する可能性があります
     }
     //console.log('lazy: unload');
-  }, options);
+  };
+  
+  eachEventListener(addEventListener);
+  if (document.readyState != 'complete') {
+    // loadイベント開始前
+    addEventListener('load', onLoad);
+  } else {
+    // loadイベント開始後
+    onLoad();
+  }
+  addEventListener('unload', onUnload);
   //console.log('lazy: init');
   
-})(window, document);
+})(document);
